8833d0a60759a2d990589b429668205858837fb5
SnowHana <wujindaniel1011@gmail.com>
2024-01-13 11:41:19 +0900

    Got market cap for every dowjones stocks

--- main.py
+++ main.py
@@ -4,8 +4,25 @@ import yfinance as yf
 sp500_url = r"https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 dow_url = r"https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average#Components"
 
-dow_table = pd.read_html(dow_url)[1]
+dow_symbols = pd.read_html(dow_url)[1]["Symbol"]
+print(dow_symbols)
 
+# ticker = yf.Ticker("AAPL")
+# print(ticker.get_info().keys())
+# print(ticker.get_fast_info().keys())
+
+for symbol in dow_symbols:
+    # print(symbol)
+    ticker = yf.Ticker(symbol)
+    print(ticker.get_fast_info()["marketCap"])
+
+# Get market cap
+# market_cap = quote_table[]
+
+# ticker = []
+# deltas = []
+# sectors = []
+# market_caps = []
 
 # print(len(tables))
 # print(pd.read_html(dow_url)[1])
