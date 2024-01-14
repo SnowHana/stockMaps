c9680153d56ccc3b0d72f5daa49f11f26ad2b065
SnowHana <wujindaniel1011@gmail.com>
2024-01-14 13:45:19 +0900

    Finished bs4 version as well, having size issue

---
+++ bs4_stock_map/main.py
@@ -0,0 +1,71 @@
+from bs4 import BeautifulSoup
+import requests
+import matplotlib.pyplot as plt
+import squarify
+
+url = "https://companiesmarketcap.com/dow-jones/largest-companies-by-market-cap/"
+
+# Create a browser
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html5lib")
+
+rows = soup.table.select("tr")
+
+# print(rows)
+
+symbols = []
+market_caps = []
+sizes = []
+for row in rows:
+    try:
+        symbol = row.find("div", {"class": "company-code"}).text
+        # print(symbol)
+        symbols.append(symbol)
+
+        market_cap = row.findAll("td")[3].text
+        # print(market_cap)
+        market_caps.append(market_cap)
+
+        # Now Clean market cap
+        multiplier = 1
+        if market_cap.endswith("T"):
+            multiplier = 10**12
+            # print(size)
+        elif market_cap.endswith("B"):
+            multiplier = 10**9
+            # print(size)
+        else:
+            print("ERROR")
+
+        size = float(market_cap[1:-2]) * multiplier
+        sizes.append(size)
+    except AttributeError:
+        pass
+
+labels = [f"{symbols[i]}\n{market_caps[i]}" for i in range(len(symbols))]
+colors = [plt.cm.tab20c(i / float(len(symbols))) for i in range(len(symbols))]
+
+squarify.plot(
+    sizes=sizes,
+    label=labels,
+    color=colors,
+    alpha=0.7,
+    bar_kwargs={"linewidth": 0.5, "edgecolor": "#111111"},
+)
+
+
+plt.title("DowMap")
+plt.savefig("dow_plot.png")
+plt.close()
+# squarify.plot(
+#     sizes=market_caps,
+#     label=labels,
+#     color=colors,
+#     bar_kwargs={"linewidth": 0.5, "edgecolor": "#111111"},
+# )
+
+# plt.title("DowMap")
+# plt.savefig("dow_plot.png")
+# plt.close()
+
+# print(labels)
